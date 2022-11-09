151fd914d1b9d1ddef3c78d335d6da627e0e4e11
prapantb <babprapant@gmail.com>
2022-11-09 22:05:47 +0100

    Update mise en place des vues accueil et jouer

--- app/src/main/java/com/example/breye/JouerActivity.java
+++ app/src/main/java/com/example/breye/JouerActivity.java
@@ -2,18 +2,25 @@ package com.example.breye;
 
 import android.os.Bundle;
 
+import com.google.android.material.snackbar.Snackbar;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import com.example.breye.databinding.ActivityJouerBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class JouerActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 }
