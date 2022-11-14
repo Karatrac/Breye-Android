138d678b53124ee064f3e2d40694a761110ffa92
prapantb <babprapant@gmail.com>
2022-11-14 10:59:42 +0100

    Update structure project and main page

--- app/src/main/java/com/example/breye/MainActivity.java
+++ app/src/main/java/com/example/breye/MainActivity.java
@@ -1,19 +1,18 @@
 package com.example.breye;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import androidx.navigation.ui.AppBarConfiguration;
-
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_main);
     }
+
 }
